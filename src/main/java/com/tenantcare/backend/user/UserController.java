b7b536752e052421f1b89582e16c40125fa53425
Kacper Krupa <kacperkrupa123@gmail.com>
2026-09-15 23:06:48 +0200

    feat: setup Swagger UI, configure Spring Security and fix JPA table generation

--- src/main/java/com/tenantcare/backend/user/UserController.java
+++ src/main/java/com/tenantcare/backend/user/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/va/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
